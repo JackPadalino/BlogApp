893a28d07a09ce83d4bfb2e33e995ce4b9f492a9
Jack <97137486+jackpadalino@users.noreply.github.com>
2022-01-31 21:40:08 -0500

    Added Comment model

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin # this import standard with Django
-from .models import Post #importing the DB class 'Post'
+from .models import Post,Comment #importing the DB class 'Post' and 'Comment'
+
 
 # Register your models here.
 admin.site.register(Post) # by adding this we are adding the Post model to our database. We can now see the Post model in the admin GUI page online
+admin.site.register(Comment)
--- blog/models.py
+++ blog/models.py
@@ -18,3 +18,15 @@ class Post(models.Model):
     
     def get_absolute_url(self):
         return reverse('post-details',kwargs={'pk':self.pk})
+
+class Comment(models.Model):
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.content} | {self.date_posted} | {self.author}'
+    
+    #def get_absolute_url(self):
+    #    return reverse('post-details',kwargs={'pk':self.pk})
