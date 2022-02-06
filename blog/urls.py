843ef9632c4511d030ea27591f89ff028d2a87e1
Jack <97137486+jackpadalino@users.noreply.github.com>
2022-02-06 17:15:20 -0500

    Updated base.html

--- blog/urls.py
+++ blog/urls.py
@@ -6,13 +6,13 @@ from .views import PostListView,PostDetailView,PostCreateView,PostUpdateView,Pos
 urlpatterns = [
     #path('',views.home,name='blog-blog'),
     path('', PostListView.as_view(),name='blog-home'),
-    path('latestposts',LatestPostsListView,name='latest-posts'),
-    path('topcomments',TopCommentsListView,name='top-comments'),
+    path('latestposts/',LatestPostsListView,name='latest-posts'),
+    path('topcomments/',TopCommentsListView,name='top-comments'),
     path('post/new/',PostCreateView.as_view(),name='post-create'),
     #path('post/<int:pk>/',PostDetailView.as_view(),name='post-details'),
     path('post/<int:pk>/',PostDetailView,name='post-details'),
     path('post/<int:pk>/update/',PostUpdateView.as_view(),name='post-update'),
     path('post/<int:pk>/delete/',PostDeleteView.as_view(),name='post-delete'),
-    path('user/<str:username>', UserPostListView.as_view(),name='user-posts'),
+    path('user/<str:username>/', UserPostListView.as_view(),name='user-posts'),
     path('post/<int:pk>/comment/',CommentCreateView.as_view(),name='add-comment'),
 ]
