5d087290a2eee219b689cae953312645b4417d6b
Jack <97137486+jackpadalino@users.noreply.github.com>
2022-05-28 01:16:11 -0400

    Updated user/models.py

--- users/models.py
+++ users/models.py
@@ -11,8 +11,8 @@ class Profile(models.Model):
         return f'{self.user.username}'
     
 
-    #def save(self, *args, **kwargs):
-    #    super(Profile, self).save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
     #    img = Image.open(self.image.path)
     #    if img.height > 300 or img.width > 300:
     #        output_size = (300,300)
